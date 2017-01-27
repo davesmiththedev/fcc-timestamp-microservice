a698adead5a487251aa92a9b2f52497b914f2779
Dave Smith <lah4life@gmail.com>
2017-01-27 22:43:28 +0000

    Update to route /:date to handle no date value in server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/:date', (req, res)=>{
-    res.end(req.params.date);
+    var date = res.params.date;
+    if(date){
+        res.end(date);
+    }else{
+        res.end('Try again with a date');
+    };
 });
 
 app.get('/', (res, req)=>{
