d9d10cc13cfd4b393ba288b480c7ae93b64b7faa
Dave Smith <lah4life@gmail.com>
2017-01-28 00:06:03 +0000

    Add date logic to route /:date in server.js

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const app = express();
+const moment = require("moment");
 
 var port = process.env.PORT || 8080;
 
 app.get('/:date', (req, res)=>{
-    var date = res.params.date;
-    if(date){
-        res.end(date);
+    var date = req.params.date
+    var naturalDate;
+    var unixDate;
+
+    if(isNaN(date)){
+        naturalDate = moment(date).format("MMM D, YYYY");
+        unixDate = moment(date).unix();
     }else{
-        res.end('Try again with a date');
+        unixDate = moment.unix(date).unix();
+        naturalDate = moment.unix(date).format("MMM D, YYYY");
+    };
+    if(unixDate){
+        res.send({unix: unixDate, natural: naturalDate}, 200);
+    }else{
+        res.send({unix: null, natural: null}, 200);
     };
 });
 
-app.get('/', (res, req)=>{
+app.get('/', (req, res)=>{
     res.end('Try again with a date');
 });
 
